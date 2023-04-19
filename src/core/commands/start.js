f49f70c7c7902a14cacec7f7c25d14d56df34658
Mohirbek Odiljonov <mahercoder@gmail.com>
2023-04-20 00:17:29 +0500

    Some changes

--- src/core/commands/start.js
+++ src/core/commands/start.js
@@ -6,30 +6,19 @@ module.exports = {
     name: `start`,
     action:
     async function(ctx){
-        User.findOrCreate({
-            where: {
-                id: ctx.from.id, 
-                first_name: ctx.from.first_name,
-                username: ctx.from.username
-            }
-        })
-        .then( ([user, created]) => {
-            ctx.session.thisUser = user.dataValues;
+        try{
+            let user = await User.findOne({ where: { id: ctx.from.id }});
             
+            if(user == null){
+                user = await User.create({ id: ctx.from.id, first_name: ctx.from.first_name, username: ctx.from.username });
+            }
+    
+            ctx.session.thisUser = user.dataValues;
+    
             ctx.scene.enter('user-signup');
-            // if(created || !user.district_number){
-            // } else {
-            //     ctx.scene.enter('user-subscription');
-            // }
-        })
-        .catch(err => {
-            logger.error(err);
+        }catch(err){
             console.log(err);
-        });
-
-        // if(helpers.isSubscribed(ctx, [])){
-        //     ctx.scene.enter('user-home');
-        // }
-
+            logger.error(err);
+        }
     }
 }
